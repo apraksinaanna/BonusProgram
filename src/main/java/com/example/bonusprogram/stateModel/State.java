e33bb0eb3d82dbe176de8afe0649969fb486f23b
Andrey <aapraxin@gmail.com>
2022-10-17 13:30:33 +0300

    Revert "init commit"
    This reverts commit a9d8fa426ed430152a154101a1e27d9a9b79e04e.

--- src/main/java/com/example/bonusprogram/stateModel/State.java
+++
@@ -1,6 +0,0 @@
-package com.example.bonusprogram.stateModel;
-
-//интерфейс с методом для перехода между состояниями
-public interface State {
-    public void doPayment(PaymentContext paymentContext);
-}
